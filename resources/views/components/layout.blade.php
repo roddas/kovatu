9cb4c44f6a47556f8b12a4b33aa687f23acc9b4e
Rodolfo Neves <roddas360@gmail.com>
2024-10-11 10:48:52 -0500

    Add the index page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="//unpkg.com/alpinejs" defer></script>
+    <link rel="icon" type="image/x-icon" href="{{ asset('logo.svg') }}">
     <title>{{ env('APP_NAME') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
-    <p class="text-3xl font-bold underline">TITL</p>
+    <div class=" bg-red-200 py-4 mx-auto flex justify-center">
+        <a href="{{ route('index'); }}"><img src="{{ asset('logo.svg') }}" alt=""></a>
+    </div>
+    <div class="max-w-screen-xl mx-auto">
+        <section class=" bg-blue-400">
+            {{ $slot }}
+        </section>
+        <footer class="bg-purple-500">footer</footer>
+    </div>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('/', IndexController::class);
 
 Route::get('/', function () {
     return view('landing');
-});
+})->name('index');
