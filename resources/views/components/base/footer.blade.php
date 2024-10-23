63e16d8e64388271ae077c4a914f3d21515f717e
Rodolfo Neves <roddas360@gmail.com>
2024-10-23 12:10:12 -0400

    feat: Add the footer component

---
+++ resources/views/components/base/footer.blade.php
@@ -0,0 +1,22 @@
+    <div class="text-primaryBlue">
+        <div class="text-center mb-5 z-100">
+            <ul class="list-none p-3">
+                <li class="md:inline mx-5">
+                    <a class="link" href="" text="Sobre"></a>
+                </li>
+                <li class="md:inline mx-5">
+                    <a class="link" href="">Contactos</a>
+                </li>
+                <li class="md:inline mx-5">
+                    <a class="link" href="#">Suporte</a>
+                </li>
+                <li class="md:inline mx-5">
+                    <a class="link" href="#">Transparência</a>
+                </li>
+                <li class="md:inline mx-5">
+                    <a class="link" href="#">Logout</a>
+                </li>
+            </ul>
+            <p>{{ date('Y') }} &copy; Ko Vatuvembela</p>
+        </div>
+    </div>
