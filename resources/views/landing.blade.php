9ab882a330a70b4919bd0e5f7a94e991b04f6e9d
Rodolfo Neves <roddas360@gmail.com>
2024-10-23 12:09:56 -0400

    feat: Enhace the landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,3 +1,54 @@
 <x-layout>
-    adasdda
+    <h1 class="text-primaryBlue title my-8 ">Um lugar para partilharmos conhecimentos e entendermos melhor as nossas
+        raízes.
+    </h1>
+    <section class="md:flex md:justify-around w-full mt-20 ">
+        <form action="" method="post" class="p-8 md:w-[50%]">
+            @csrf
+            {{-- Email --}}
+            <div class="mb-4">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" value="{{ old('email') }}"
+                    placeholder="email@domain.com" required>
+                @error('email')
+                    <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
+
+            {{-- Password --}}
+            <div class="mb-4">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="********************" required>
+                @error('password')
+                    <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
+
+            {{-- Remember checkbox --}}
+            <div class="mb-4">
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Lembrar-me</label>
+            </div>
+            {{-- Submit button --}}
+            <button type="submit" class="">Entrar</button>
+            <p class="my-4">
+                <a href="#" class="link my-2">Esqueceu a senha ?</a>
+            </p>
+            <a href="#" class="link mb-4">Criar uma conta</a>
+
+        </form>
+        
+        {{-- Quotes side --}}
+        <section class="my-auto">
+            <img src="{{ asset('images/quotes.svg') }}" width="56" alt="">
+            <div class="flex justify-center text-center">
+                <p class="text-5xl font-thin text-primaryBlue w-[80%] text-justify italic">Yamwangana ixi kuswa</p>
+            </div>
+            <img src="{{ asset('images/quotes.svg') }}" width="56" alt=""
+                class="rotate-180 float-right mb-5">
+            <p class=" text-base text-primaryBlue mt-12 ml-5">
+                - Ditado popular Cokwé
+            </p>
+        </section>
+    </section>
 </x-layout>
