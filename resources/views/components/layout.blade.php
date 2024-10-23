92849d30e71b1b39576251f7667d63b5d03bcc53
Rodolfo Neves <roddas360@gmail.com>
2024-10-23 12:10:31 -0400

    feat: Enhace the landing page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,22 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="//unpkg.com/alpinejs" defer></script>
+    {{-- SLOW INTERNET, AFTER RESOLVE THIS PROBLEM, CHANGE BACK --}}
+    {{-- <script src="https://unpkg.com/alpinejs" defer></script> --}}
+    <script src="https://unpkg.com/alpinejs@3.14.3/dist/cdn.min.js" defer></script>
     <link rel="icon" type="image/x-icon" href="{{ asset('logo.svg') }}">
     <title>{{ env('APP_NAME') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
-    <div class=" bg-red-200 py-4 mx-auto flex justify-center">
+    <div class=" py-4 mx-auto flex justify-center">
         <a href="{{ route('index'); }}"><img src="{{ asset('logo.svg') }}" alt=""></a>
     </div>
     <div class="max-w-screen-xl mx-auto">
-        <section class=" bg-blue-400">
+        <section class=" ">
             {{ $slot }}
         </section>
-        <footer class="bg-purple-500">footer</footer>
     </div>
+    <x-base.footer></x-base.footer>
 </body>
 
-</html>
+</html> 
