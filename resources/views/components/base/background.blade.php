ab0e0b259cf5fcfe75d2ff4d1195f1f2958035ec
Rodolfo Neves <roddas360@gmail.com>
2024-10-23 12:44:43 -0400

    simple up0dates

--- resources/views/components/base/background.blade.php
+++ resources/views/components/base/background.blade.php
@@ -1,9 +1,4 @@
 <div class="flex-row-reverse flex">
-    <Image
-        alt="Background Image"
-        src="{{ asset('/images/' . $source . '.svg') }}"
-        width="500"
-        height="540"
-        class="opacity-30 blur-sm fixed -z-10 right-0 bottom-1"
-    />
+    <img alt="Background Image" src="{{ asset('/images/' . $source . '.svg') }}" width="500" height="540"
+        class="opacity-30 blur-sm fixed -z-10 right-0 bottom-1" />
 </div>
