6edcd72ef3c1a5dfe2b671786b383993fa5282fa
Rodolfo Neves <roddas360@gmail.com>
2024-11-08 06:32:30 -0500

    feat: Add the signup route file

--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,63 @@
 
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilizadorController;
+
 
 // Index page
 Route::get('/', [IndexController::class, 'index'])->name('index');
 Route::post('/', [IndexController::class, 'store'])->name('index');
 
-// About page
-Route::get('/public/about', function () {
-    return view('public/about');
-})->name('about');
+Route::prefix('public')->group(function () {
 
-// Contact's page
-Route::get('/public/contacts', function () {
-    return view('public/contacts');
-})->name('contacts');
+    // About page
+    Route::get('about', function () {
+        return view('public/about');
+    })->name('about');
 
-// Support's page
-Route::get('/public/support', function () {
-    return view('public/support');
-})->name('support');
+    // Contact's page
+    Route::get('contacts', function () {
+        return view('public/contacts');
+    })->name('contacts');
+
+    // Support's p
+    // Index page
+    Route::get('/', [IndexController::class, 'index'])->name('index');
+    Route::post('/', [IndexController::class, 'store'])->name('index');
+
+    // About page
+    Route::get('/public/about', function () {
+        return view('public/about');
+    })->name('about');
+
+    // Contact's page
+    Route::get('/public/contacts', function () {
+        return view('public/contacts');
+    })->name('contacts');
+
+    // Support's page
+    Route::get('/public/support', function () {
+        return view('public/support');
+    })->name('support');
+
+    // The last one
+    Route::fallback(function () {
+        return view('landing');
+    });
+
+    Route::get('support', function () {
+        return view('public/support');
+    })->name('support');
+
+    // Create user page view
+    Route::get('signup', function () {
+        return view('public/signup');
+    })->name('signup');
+
+    Route::post('signup', [UtilizadorController::class, 'store'])->name('signup');
+});
 
 // The last one
 Route::fallback(function () {
     return view('landing');
 });
-
-// Route::get('/', function () {
-//     return view('landing');
-// })->name('index');
