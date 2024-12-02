6b1c66ef63862e3e70a6b84fa889843b52b0db5b
Rodolfo Neves <roddas360@gmail.com>
2024-12-02 08:34:46 -0500

    add the default seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Utilizador;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Utilizador::updateOrCreate([
+            'email' => 'fakemail@email.com',
+            'nome' => 'Cykolomwenyo',
+            'sobrenome' => 'Mulemvu',
+            'password' => '123456',
         ]);
     }
 }
