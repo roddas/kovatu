180f32c65a4f9ded18b071590e5d29fbc16458e6
Rodolfo Neves <roddas360@gmail.com>
2025-01-08 06:11:36 -0500

    checkpoint

--- database/seeders/ProverbioSeeder.php
+++ database/seeders/ProverbioSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\QuotesModel as ProverbioModel;
+use App\Models\Utilizador;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\App;
-
+use PHPUnit\TextUI\XmlConfiguration\Logging\Logging;
 
 class ProverbioSeeder extends Seeder
 {
@@ -15,6 +17,7 @@ class ProverbioSeeder extends Seeder
     public function run(): void
     {
         if (App::environment('local')) {
+            $userId = DB::table('utilizador')->select('uid')->where('email', 'fakemail@email.com')->get()[0]->uid;
             $proverbios = [];
         }
     }
